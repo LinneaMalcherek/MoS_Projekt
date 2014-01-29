61578ee0b5f24fe02349f945b5b641767fb67256
unknown <NaiLi>
2014-01-29 13:37:11 +0100

    kommentarer och test att rita ut rörelse

--- test1.m
+++ test1.m
@@ -1,7 +1,8 @@
 % Skickar in en kraft, massa, tid, friktionskoefficient, radie
 F = 12; 
 m = 18;
-t = 1:100;
+Tmax = 100;
+t = 1:Tmax;
 f = 0.0168;
 F_friktion = f*m*9.82;
 r = 0.1454676;
@@ -23,16 +24,17 @@ v_back = v_front*0.3;
 
 %%
 % Resultant
-forw = zeros(2,100);
+forw = zeros(2,Tmax);
 forw(2,:) = v_forw;
-sidew = zeros(2,100);
+sidew = zeros(2,Tmax);
 sidew(1,:) = (v_back-v_front);
 v = forw + sidew;
 
 %% Beräkna position
+% Skapar en positionsvektor och placerar koordinater för x och y
 p0 = [0;0];
-pos = zeros(2,100);
-for i = 1:1:100
+pos = zeros(2,Tmax);
+for i = 1:1:Tmax
     pos(:,i) = p0+v(:,i);
     p0 = pos(:,i); 
     if v(2,i)<0.001
@@ -44,7 +46,20 @@ for i = 1:1:100
 end
 
 %%
-plot(pos(1,:),pos(2,:),'*')
-axis equal
+i=1;
+while i<80
+    plot(pos(1,i),pos(2,i),'o')
+    axis([-10 10 0 50])
+    drawnow;
+    pause(0.05);
+    hold on;
+    i = i+1;
+end
+
+% %
+% while 
+% plot(pos(1,:),pos(2,:),'*')
+% drawnow;
+% axis equal
 
 
