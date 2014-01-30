44bd69d5bc453a87316909569e1ec8357b576363
NaiLi <NaiLi>
2014-01-30 13:36:56 +0100

    Ändrat till utslagsvektor. Kommenterat.

--- test1.m
+++ test1.m
@@ -1,70 +1,157 @@
-%% input
-v0 = 3.5;
-vinkel = 0;
-% Skickar in en kraft, massa, tid, friktionskoefficient, radie
-% F = 12; 
-Rotationmatrix = [cos(vinkel) -sin(vinkel);
-                  sin(vinkel) cos(vinkel)];
-% utgangsriktningsvektor = [1; 0] * Rotationsmatrix;
+%% Curlingvisualisering MoS-projekt 2014
+%  Grupp 3
+%  
+%% Input
+v0 = 1.223;
+angle = -pi/28;
+
+% Utslagsriktning beräknas
+rotationmatrix = [cos(angle) -sin(angle);
+                  sin(angle) cos(angle)];
+direction = rotationmatrix*[0;1];
+direction = direction/(sqrt(direction(1,1)*direction(1,1)+direction(2,1)*direction(2,1)));
+% Ortogonal riktning beräknas
+direction_ort = [cos(pi/2) -sin(pi/2);sin(pi/2) cos(pi/2)]*direction;
+
+%% Fasta data
+g = 9.82;
+% Sten
 m = 18;
-t = 1:0.1:40;
-f = 0.0168;
-F_friktion = f*m*9.82;
 r = 0.1454676;
-% Beräknar utslagshastighet
-v0 = 3.5; %integral(F) / m; 
+r_inner = 0.1;
+% Isen
+my = 0.0168;
+my_rot = 0.0068; % Kolla denna... 
+F_friktion = my*m*g;
+F_friktion_rotation=my_rot*m*g;
+% Banan
+length = 36.59;                 % Från hack till banans slut
+width = 20;                     % Banans bredd
+hackhog = 6.40;                 % Från hack till hog
+tee = [0;34.76];                % Koordinater för mitten av bot
+% Tid och startposition och startvinkelhastighet(abs)
+t = 1:0.1:40;
+position = [0;0];
+
 % Beräknar tid mellan hack och hogg
-s = 6.40; % Avstånd hack och hogg
-t0 = s/v0;
+t0 = hackhog/v0;
 
-% om siktar åt vänster, rotera åt höger och vice versa
-if vinkel>0
-    omega = -pi / (2*t0);
-elseif vinkel<=0
-    omega = pi / (2*t0);
+% Eftersom vi aldrig använder vinkelhastighetens riktning och det blir
+% fel i beräkningen av omega längre fram kan startvinkelhastigheten lika
+% gärna vara positiv (absbelopp) från början
+omega0 = pi / (2*t0); 
+v0_p = omega0*r_inner;
+
+% Ange friktionsriktningar beroende på sikte
+if  (-pi/2)<=angle<=0 % Curlar åt vänster (sikte höger)
+    %omega0 = -pi / (2*t0); %(flyttat upp) 
+    c1 = 0.00001;
+    c2 = -0.001;
+elseif 0<angle<=(pi/2) % Curlar åt höger (sikte vänster)
+    %omega0 = pi / (2*t0);
+    c1 = -0.00001;
+    c2 = 0.001;
 end
 
-%%
-% Beräknar momentanhastigheten framåt
-v_forw = (v0 - (F_friktion * t / m));
-v_front = omega*r; %Testar nu för outhand
-v_back = omega*r*0.1; %Testar nu för outhand
-v_front = r*omega^3 / 3*m; %Beräknat utifrån centrifugalkraften
-v_back = v_front*0.3; 
-
-%%
-% Resultant
-forw = zeros(2,size(t,2));
-forw(2,:) = v_forw;
-sidew = zeros(2,size(t,2));
-sidew(1,:) = (v_back-v_front);
-v = forw + sidew;
-
-%% Beräkna position
-% Skapar en positionsvektor och placerar koordinater för x och y
-p0 = [0;0];
-pos = zeros(2,size(t,2));
-for i = 1:1:100
-    pos(:,i) = p0+v(:,i);
-    p0 = pos(:,i); 
-    if v(2,i)<0.001
+%% Beräkna och rita position för varje tid t
+for t = 1:0.1:40
+    % Beräknar momentanhastigheten framåt i vektor
+    v_forw = v0 - (F_friktion * t / m);
+    % Beräknar momentanvinkelhastigheten i vektor
+    %omega = omega0 - (F_friktion_rotation*t / m) %Friktionen är för hög nu. 
+    v_p = v0_p - F_friktion_rotation*t/m;
+    omega = v_p/r_inner;
+    % Kontrollera att hastigheten inte är negativ
+    if v_forw < 0% || omega < 0 %(Går inte nu med omega, är neg pga friktionen är för stor) 
         break;
     end
-    if pos(2,i) > 36.59
-        break;
-    end
-end
-
-%%
-i=1;
-while i<80
-    plot(pos(1,i),pos(2,i),'o')
-    axis([-10 10 0 50])
+    
+    % Kraft och hastighet i sidled
+    F_side = (m*g*(c1+c2))/sqrt(abs(omega)*r);
+    v_side =  F_side*t/m;
+    
+    % Resultant
+    v = v_forw*direction + v_side*direction_ort;
+    position = position + v;%rotationmatrix*v;
+    
+    % Rita
+    plot(position(1,1),position(2,1),'o')
+    axis([-20 20 0 40])
     drawnow;
     pause(0.05);
     hold on;
-    i = i+1;
+    if position(2,1) > length % Om stenen går utanför på längden
+        break;
+    end
+    
 end
+
+%Skriv ut position och hur långt ifrån tee stenen hamnat
+position
+distance = sqrt((tee(1,1)-position(1,1))^2+(tee(2,1)-position(2,1))^2)
+
+% %%
+% % Beräknar momentanhastigheten framåt i vektor
+% v_forw = v0 - (F_friktion * t / m);
+% % Beräknar momentanvinkelhastigheten i vektor
+% omega = omega0 - (F_friktion_rotation*t / m); 
+% % Kraft och hastighet i sidled
+% F_side = (m*g*(c1+c2))/sqrt(abs(omega)*r);
+% v_side =  F_side*t/m;
+
+%%
+% % Resultant
+% forw = zeros(2,size(t,2));
+% forw(2,:) = v_forw;
+% sidew = zeros(2,size(t,2));
+% sidew(1,:) = v_side;
+% v = forw + sidew;
+
+% %% Beräkna position
+% % Skapar en positionsvektor och placerar koordinater för x och y
+% % p0 = [0;0];
+% position = [0 0];
+% % pos = zeros(2,size(t,2));
+% for i = 1:1:size(t,2)
+%     %pos(:,i) = (p0+v(:,i));
+%     position=position + v(:,i)'*rotationmatrix;
+%    % pos(:,i) = pos(:,i)'*rotationmatrix
+%     %position = position*rotationmatrix;
+%     %p0 = pos(:,i); 
+%     
+%     %%---------
+%     plot(position(1,1),position(1,2),'o')
+%     axis([-10 10 0 50])
+%     drawnow;
+%     pause(0.05);
+%     hold on;
+%     %%--------
+%     if v(2,i)<0.001 % Om stenen stannar
+%         v(2,i)
+%         break;
+%     end
+%     if position(1,2) > length % Om stenen går utanför på längden
+%         position(1,2) 
+%         break;
+%     end
+% %     if abs(position(1,1)) < (width/2) % Om stenen går utanför åt sidorna
+% %         break;
+% %     end
+% end
+
+%%
+% i=1;
+% while i<80
+%     plot(pos(1,i),pos(2,i),'o')
+%     axis([-10 10 0 50])
+%     drawnow;
+%     pause(0.05);
+%     hold on;
+%     i = i+1;
+%     if pos(1,i) == 0 && pos(2,i) == 0
+%         break;
+%     end
+% end
 
 % %
 % while 
