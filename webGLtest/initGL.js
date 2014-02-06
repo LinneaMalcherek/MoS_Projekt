df527897f79e4b3d937eb022c12ff6f27eda1703
Michael <michaelnilsson90@hotmail.com>
2014-02-06 13:00:13 +0100

    Michael och Linnea testar webGL.

---
+++ webGLtest/initGL.js
@@ -0,0 +1,12 @@
+ var gl;
+    function initGL(canvas) {
+        try {
+            gl = canvas.getContext("webgl");
+            gl.viewportWidth = canvas.width;
+            gl.viewportHeight = canvas.height;
+        } catch (e) {
+        }
+        if (!gl) {
+            alert("Could not initialise WebGL, sorry :-(");
+        }
+    }
