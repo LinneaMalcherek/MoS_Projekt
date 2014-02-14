9cc9936dc17264636b3e92da6f0cd8aee1bc5b05
Julia Nilsson <julianilsson92@hotmail.com>
2014-02-14 16:24:54 +0100

    collisiontest ändrat lite så det typ ser bra ut iaf

--- Matlabsimulering/collisiontest.m
+++ Matlabsimulering/collisiontest.m
@@ -1,26 +1,30 @@
-function [ speed1, direction_forw1, speed2, direction_forw2 ] = collision( stone_pos1,stone_pos2, velocity )
+function [ speed1, direction_forw1, speed2, direction_forw2 ] = collisiontest( stone_pos1,stone_pos2, velocity )
 % Calculate the new directions and speed after a collision between two
 % stones, stone 1 is the approaching stone.
 % velocity = velocity of stone 1 before collision
-        
-        e=0.9;
+        e=0.6;
+        k=1-e;
         
         direction_forw2=stone_pos2-stone_pos1;
         
+        %Projecering %velocity hastighetsvektorer (hastighet i två riktningar)
         velocity2=((velocity'*direction_forw2)/(direction_forw2'*direction_forw2))*direction_forw2;
         velocity1=velocity-velocity2;
         
-        speed2=(velocity-e*(velocity))/2;
-        
-        speed1=speed2-e*velocity;
-        
-        speed1= sqrt( power(speed(1,1),2) + power(speed1(2,1),2));
-        speed1a= sqrt( power(velocity1(1,1),2) + power(velocity1(2,1),2));
-        direction_forw1 = velocity1 / speed1a;
-        
-        speed2= sqrt( power(speed2(1,1),2) + power(speed2(2,1),2));
-        speed2a= sqrt( power(velocity2(1,1),2) + power(velocity2(2,1),2));
-        direction_forw2 = direction_forw2 / speed2a;
+        %Speed - skalär, EN hastighet i EN riktning
+        speed=sqrt( power(velocity(1,1),2) + power(velocity(2,1),2)); %Inkommande stenens speed
+           
+        speed2=speed-k*speed;
+        speed1=speed2-k*speed;
+       
+        %speed1= sqrt( power(velocity1(1,1),2) + power(velocity1(2,1),2));
+        %detta hade vi innan vid elastisk
+        direction_forw1 = velocity1 / speed1;
+       
+        %speed2= sqrt( power(velocity2(1,1),2) + power(velocity2(2,1),2));
+        %detta hade vi innan vi elastisk
+        %direction_forw2 = direction_forw2 / speed2; Hur kunde vi ha detta innan?
+        direction_forw2 = velocity2 / speed2;
 
 end
 
