9b2d0cbd0e525d48cdd167b69ec19e9a1aa6e009
Julia Nilsson <julianilsson92@hotmail.com>
2014-02-20 16:05:31 +0100

    inelastisk krock nastan klar

--- Matlabsimulering/inelastic_collision.m
+++ Matlabsimulering/inelastic_collision.m
@@ -1,7 +1,7 @@
-function [ velocity1_after, velocity2_after ] = inelastic_collision( stone_pos1, stone_pos2, velocity1, velocity2 )
+function [ speed1, direction_forw1, speed2, direction_forw2 ] = inelastic_collision( stone_pos1, stone_pos2, velocity1, velocity2 )
 
 %Söta stötkoeficienten 
-e=0.8;
+e=0;
 
 collision_normal=stone_pos2-stone_pos1;
 
@@ -16,6 +16,12 @@ velocity2_after_normal=(velocity1_normal+velocity2_normal+e*(velocity1_normal-ve
 
 velocity1_after=velocity1_after_normal+velocity1_tangent;
 velocity2_after=velocity2_after_normal+velocity2_tangent;
+
+speed1=sqrt( power(velocity1_after(1,1),2) + power(velocity1_after(2,1),2))
+speed2=sqrt( power(velocity2_after(1,1),2) + power(velocity2_after(2,1),2))
+
+direction_forw1 = velocity1_after / speed1;
+direction_forw2 = velocity2_after / speed2;
 
 end
 
