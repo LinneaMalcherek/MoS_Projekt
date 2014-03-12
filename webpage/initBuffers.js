1848ee4dfed2a3242b05eae34247e11ecab36188
Michael <michaelnilsson90@hotmail.com>
2014-03-12 10:35:57 +0100

    fixed stone issue

--- webpage/initBuffers.js
+++ webpage/initBuffers.js
@@ -1,6 +1,6 @@
 var VertexPositionBuffer;
 var VertexTextureCoordBuffer;
-var VertexIndexBuffer;
+//var VertexIndexBuffer;
 
 var VertexPositionBuffer2;
 var VertexTextureCoordBuffer2;
@@ -14,21 +14,21 @@ function handleLoadedObject(objectData) {
 
     VertexTextureCoordBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, VertexTextureCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(objectData.texcoords), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(objectData.mTextureCoords), gl.STATIC_DRAW);
     VertexTextureCoordBuffer.itemSize = 2;
-    VertexTextureCoordBuffer.numItems = objectData.texcoords.length / 2;
+    VertexTextureCoordBuffer.numItems = objectData.mTextureCoords.length / 2;
 
     VertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, VertexPositionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(objectData.verts), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(objectData.mVertexPoints), gl.STATIC_DRAW);
     VertexPositionBuffer.itemSize = 3;
-    VertexPositionBuffer.numItems = objectData.verts.length / 3;
+    VertexPositionBuffer.numItems = objectData.mVertexPoints.length / 3;
 
-    VertexIndexBuffer = gl.createBuffer();
+    /*VertexIndexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, VertexIndexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(objectData.indices), gl.STATIC_DRAW);
     VertexIndexBuffer.itemSize = 1;
-    VertexIndexBuffer.numItems = objectData.indices.length;
+    VertexIndexBuffer.numItems = objectData.indices.length;*/
 
 }
 
@@ -79,7 +79,7 @@ function handleLoadedObject3(objectData) {
 
 function loadObject() {
     var request = new XMLHttpRequest();
-    request.open("GET", "stone.json"); <!-- vilket objekt man vill läsa in -->
+    request.open("GET", "stone_2.json"); <!-- vilket objekt man vill läsa in -->
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
             handleLoadedObject(JSON.parse(request.responseText));
